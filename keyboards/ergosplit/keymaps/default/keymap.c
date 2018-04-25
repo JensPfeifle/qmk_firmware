794fade73878f037d3d196f2063c68e835c1b458
Jens Pfeifle <jens.pfeifle@gmail.com>
2018-04-25 22:07:56 +0200

    activate tapping and update layout

--- keyboards/ergosplit/keymaps/default/keymap.c
+++ keyboards/ergosplit/keymaps/default/keymap.c
@@ -13,6 +13,12 @@ extern keymap_config_t keymap_config;
 #define _LOWER 1
 #define _RAISE 2
 
+#define TAPPING_TERM 200
+//Tap Dance Declarations
+//enum {
+//s};
+/* https://docs.qmk.fm/features/tap-dance#tap-dance:-a-single-key-can-do-3-5-or-100-different-things */
+
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   LOWER,
@@ -42,16 +48,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_TAB ,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      DE_Z,   KC_U,   KC_I,     KC_O,    KC_P,    DE_UE, \
    KC_F5  ,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,   KC_J,   KC_K,     KC_L,    DE_OE,   DE_AE, \
    DE_LESS,  DE_Y,    KC_X,    KC_C,    KC_V,    KC_B,   KC_BSPC, KC_ENT,   KC_N,   KC_M,  KC_COMM,  KC_DOT, DE_MINS, KC_PGDN, \
-   XXXXXXX, KC_LCTL, KC_LALT, KC_LCMD,  LOWER,           KC_LSFT, KC_SPC,           RAISE, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+   KC_LCTL, KC_LALT, KC_LCMD, KC_LSFT, LOWER,           KC_LSFT, RSFT_T(KC_SPC),           RAISE, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+
 ),
 
 /* Lower */
 [_LOWER] = LAYOUT(
    DE_CIRC, DE_EXLM ,DE_DQOT, DE_HASH, DE_DLR , DE_PERC,                   DE_AMPR, DE_SLSH, _______, _______, DE_EQL , _______, \
-   _______,  DE_AT , _______, DE_EURO, DE_LESS, DE_MORE,                   _______, DE_LBRC, DE_RBRC, _______, _______, _______, \
-   _______, _______, _______, _______, DE_QST , _______,                   DE_HASH, DE_LPRN, DE_RPRN, _______, _______, DE_QUOT, \
-   _______, _______, _______, _______, DE_EXLM, _______,                   _______, DE_LCBR, DE_RCBR, _______, _______, _______, \
-   _______, _______, _______, _______,   LOWER, _______, _______, _______, _______,  RAISE , _______, _______, _______, _______ \
+   _______,  DE_AT , _______, DE_EURO, DE_LESS, DE_MORE,                   _______, DE_LBRC, DE_RBRC, DE_ASTR, _______, _______, \
+   _______, _______, _______, _______, DE_QST , DE_QUOT,                   DE_HASH, DE_LPRN, DE_RPRN, _______, _______, DE_QUOT, \
+   _______, _______, _______, _______, DE_EXLM, _______, KC_DEL , _______, _______, DE_LCBR, DE_RCBR, _______, _______, _______, \
+   KC_LCTL, KC_LALT, KC_LSFT, KC_LCMD,   LOWER,          _______, _______,           RAISE , _______, _______, _______, _______ \
 ),
 /*
 [_LOWER] = LAYOUT( \
@@ -65,10 +72,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Raise */
 [_RAISE] = LAYOUT(
    KC_F12 ,  KC_F1 ,  KC_F2 ,  KC_F3 ,  KC_F4 ,  KC_F5 ,                    KC_F6 ,  KC_F7 ,  KC_F8 ,  KC_F9 ,  KC_F10, KC_VOLU, \
-   _______, _______, _______,  KC_UP , _______, _______,                   KC_PGUP,  KC_P7 ,  KC_P8 ,  KC_P9 , KC_PMNS,  KC_F11, \
-   _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______,                   KC_HOME,  KC_P4 ,  KC_P5 ,  KC_P6 , KC_PPLS,  KC_F12, \
-   _______, _______, _______, _______, _______, _______,                   KC_PGDN,  KC_P1 ,  KC_P2 ,  KC_P3 , KC_PENT, KC_VOLD, \
-   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  KC_P0 , KC_MPRV, KC_MPLY, KC_MNXT \
+   _______, _______, _______,  KC_UP , _______, _______,                   KC_PGUP,  KC_7 ,  KC_8 ,  KC_9 , KC_PMNS,  KC_F11, \
+   _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______,                   KC_HOME,  KC_4 ,  KC_5 ,  KC_6 , KC_PPLS,  KC_F12, \
+   _______, _______, _______, _______, _______, _______, KC_DEL, _______, KC_PGDN,  KC_1 ,  KC_2 ,  KC_3 , KC_PENT, KC_VOLD, \
+   KC_LCTL, KC_LALT, KC_LCMD, KC_LSFT,   LOWER,         _______, _______,            RAISE,  KC_P0 , KC_MPRV, KC_MPLY, KC_MNXT \
 )
 
 /*[_RAISE] = LAYOUT( \
@@ -77,10 +84,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
 )*/
-
-
-
-
 
 };
 
--- keyboards/ergosplit/rules.mk
+++ keyboards/ergosplit/rules.mk
@@ -65,6 +65,7 @@ AUDIO_ENABLE = no           # Audio output on port C6
 UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.  Do not enable this with audio at the same time.
+TAP_DANCE_ENABLE=yess
 SUBPROJECT_rev1 = yes
 USE_I2C = yes
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE

d4735ad44a63772cd5321bd860191e9ffbaa043b
Jens Pfeifle <jens.pfeifle@gmail.com>
2018-04-25 21:15:57 +0200

    fixed final issues
    matrix definition was not correct

--- keyboards/ergosplit/ergosplit.h
+++ keyboards/ergosplit/ergosplit.h
@@ -27,16 +27,16 @@
 	) \
     /* matrix positions */ \
 	{ \
-		{   L00, L01, L02, L03, L04, L05,  KC_NO }, \
-		{   L10, L11, L12, L13, L14, L15,  KC_NO }, \
-		{   L20, L21, L22, L23, L24, L25,  KC_NO }, \
-		{   L30, L31, L32, L33, L34, L35,    L36 }, \
-        {   L40, L41, L42, L43, L44, KC_NO,  L46 }, \
-		{ KC_NO, R05,   R04, R03, R02, R01, R00 }, \
-		{ KC_NO, R15,   R14, R13, R12, R11, R10 }, \
-		{ KC_NO, R25,   R24, R23, R22, R21, R20 }, \
-		{ R36,   R35,   R34, R33, R32, R31, R30 }, \
-        { R46,   KC_NO, R41, R42, R43, R44, R45 } \
+		{ L00, L01, L02, L03, L04, L05,   KC_NO}, \
+		{ L10, L11, L12, L13, L14, L15,   KC_NO}, \
+		{ L20, L21, L22, L23, L24, L25,   KC_NO}, \
+		{ L30, L31, L32, L33, L34, L35,   L36}, \
+        { L40, L41, L42, L43, L44, KC_NO, L46}, \
+		{ R05, R04, R03, R02, R01, R00,   KC_NO }, \
+		{ R15, R14, R13, R12, R11, R10,   KC_NO }, \
+		{ R25, R24, R23, R22, R21, R20,   KC_NO }, \
+		{ R35, R34, R33, R32, R31, R30,   R36 }, \
+        { R45, R44, R43, R42, R41, KC_NO, R46 } \
 	}
 
 #define LAYOUT_ergosplit LAYOUT

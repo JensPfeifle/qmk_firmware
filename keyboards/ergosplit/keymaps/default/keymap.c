08e20703cb07183d490f74f4fb8259474d7d6939
Jens Pfeifle <jens.pfeifle@gmail.com>
2018-06-10 20:53:15 +0200

    updated keymap

--- keyboards/ergosplit/keymaps/default/keymap.c
+++ keyboards/ergosplit/keymaps/default/keymap.c
@@ -37,25 +37,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------------'
  */
   [_QWERTZ] = LAYOUT( /* Basic QWERTY */ 
-        KC_ESC,   DE_CIRC,    KC_1,    KC_2,    KC_3,  KC_4,   KC_5,              KC_6,    KC_7,    KC_8,    KC_9,    KC_0, DE_SS, DE_ACUT, 
+        KC_ESC,    KC_1,    KC_2,    KC_3,  KC_4,   KC_5,   KC_6,             KC_7,    KC_8,    KC_9,    KC_0, DE_SS, DE_ACUT, DE_CIRC,
         KC_TAB,        DE_Q,    DE_W,    DE_E,    DE_R,    DE_T,           DE_Z,    DE_U,    DE_I,    DE_O,    DE_P,    DE_UE, DE_PLUS, 
         KC_CAPS,        DE_A,    DE_S,    DE_D,    DE_F,    DE_G,           DE_H,    DE_J,    DE_K,    DE_L,    DE_OE,   DE_AE, DE_HASH, 
         KC_LSFT, DE_LESS, DE_Y,    DE_X,    DE_C,    DE_V,           DE_B,    DE_N,    DE_M,    DE_COMM, DE_DOT,  DE_MINS,    KC_DEL, 
-        KC_LCTL, KC_LGUI, KC_LALT, LOWER,   KC_BSPC,  KC_ENT,         KC_RSFT, KC_SPC, RAISE, KC_DEL, KC_RALT, KC_RGUI, KC_RCTL 
-),
-  [_LOWER] = LAYOUT( /* Basic QWERTY */
-        ______, ______, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
-        ______, ______,______, ______, ______, ______ ,         ______, KC_PSCR, KC_SLCK, KC_PAUS, KC_UP, ______, ______, \
-        ______, KC_VOLD,KC_VOLU,KC_MUTE,______, ______,         KC_PAST,KC_PSLS,KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT, KC_INS,  \
-        ______, ______, KC_MPRV,KC_MPLY,KC_MNXT,______,         ______, KC_PPLS,KC_PMNS,KC_END,  KC_PGDN, KC_DOWN, KC_DEL, \
-        ______, ______, ______, ______, ______, ______,         ______, ______, ______, ______, ______, ______, ______ \
+        KC_LCTL, KC_LGUI, KC_LALT, LOWER,   KC_BSPC,  KC_ENT,         KC_RSFT, KC_SPC, RAISE, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT
+  ),
+ [_LOWER] = LAYOUT( /* Basic QWERTY */
+        ______, KC_F1, KC_F2, KC_F3, KC_F4,  KC_F5, KC_F6,         KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, ______, \
+        ______, DE_AT ,KC_UP, DE_EURO, ______, ______ ,         DE_QUOT, DE_LPRN, DE_RPRN, ______, ______, ______, DE_TILD, \
+        ______, KC_LEFT,KC_DOWN,KC_RIGHT, DE_QST, ______,         DE_DQOT,DE_LBRC,DE_RBRC, ______, ______, ______, ______,  \
+        ______, DE_PIPE, ______,______,______,______,         DE_BSLS, DE_LCBR, DE_RCBR,______,______, ______, KC_INS,\
+        ______, ______, ______, ______, KC_DEL, ______,         ______, ______, ______, ______, ______, ______, ______ \
 ),
   [_RAISE] = LAYOUT( /* Basic QWERTY */
-        ______, ______, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
-        ______, ______,______, ______, ______, ______ ,         ______, KC_PSCR, KC_SLCK, KC_PAUS, KC_UP, ______, ______, \
-        ______, KC_VOLD,KC_VOLU,KC_MUTE,______, ______,         KC_PAST,KC_PSLS,KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT, KC_INS, \
-        ______, ______, KC_MPRV,KC_MPLY,KC_MNXT,______,         ______, KC_PPLS,KC_PMNS,KC_END,  KC_PGDN, KC_DOWN, KC_DEL, \
-        ______, ______, ______, ______, ______, ______,         ______, ______, ______, ______, ______, ______, ______ \
+        ______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,         KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_PAUS, \
+        ______, ______,KC_UP, ______, ______, ______ ,         ______, ______, ______, ______, KC_PGUP, ______, ______, \
+        ______, KC_LEFT,KC_DOWN,KC_RIGHT, ______, ______,         ______,______, ______, KC_HOME, KC_PGDN, KC_END, ______,\
+        ______, ______, KC_MPRV,KC_MPLY,KC_MNXT, ______,        ______, ______, ______,______, ______, ______, KC_DEL, \
+        ______, ______, ______, ______, KC_DEL, ______,         ______, ______, ______, KC_DEL, ______, ______, ______ \
 )
 };
 

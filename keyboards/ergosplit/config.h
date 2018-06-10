04fc62ae3ed49ca82dc3491cf60a98520524a6e4
Jens Pfeifle <jens.pfeifle@gmail.com>
2018-06-10 17:27:59 +0200

    working split v2

--- keyboards/ergosplit/config.h
+++ keyboards/ergosplit/config.h
@@ -25,19 +25,19 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x3060
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    JensP  
+#define MANUFACTURER    Jens Pfeifle
 #define PRODUCT         Ergosplit
-#define DESCRIPTION     A split keyboard for...me
+#define DESCRIPTION     A split 70%.
 
-/* key matrix size */
+/* key matrix size */   
 // Rows are doubled-up
 #define MATRIX_ROWS 10
 #define MATRIX_COLS 7
 
 // wiring of each half
-#define MATRIX_ROW_PINS { C6, D7, E6, B5, D4 }
-#define MATRIX_COL_PINS {F6, B1, F7, B3, B2, B6, F5}
-// #define MATRIX_COL_PINS { B6, B2, B3, B1, F7, F6 } //uncomment this line and comment line above if you need to reverse left-to-right key order
+#define MATRIX_ROW_PINS { D1, D4, C6, D7, E6 }
+//#define MATRIX_COL_PINS {B6, B3, F7, F5, F6, B1, B2}
+#define MATRIX_COL_PINS {B2, B1, F6, F5, F7, B3, B6}
 
 /* define if matrix has ghost */
 //#define MATRIX_HAS_GHOST
--- keyboards/ergosplit/keymaps/default/keymap.c
+++ keyboards/ergosplit/keymaps/default/keymap.c
@@ -9,97 +9,64 @@ extern keymap_config_t keymap_config;
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
-#define _QWERTY 0
+#define _QWERTZ 0
 #define _LOWER 1
 #define _RAISE 2
 
-#define TAPPING_TERM 200
-//Tap Dance Declarations
-//enum {
-//s};
-/* https://docs.qmk.fm/features/tap-dance#tap-dance:-a-single-key-can-do-3-5-or-100-different-things */
-
 enum custom_keycodes {
-  QWERTY = SAFE_RANGE,
-  LOWER,
-  RAISE
+  LOWER, 
+  RAISE 
 };
 
 // Fillers to make layering more clear
-#define _______ KC_TRNS
+#define ______ KC_TRNS
 #define XXXXXXX KC_NO
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-/* Qwerty
- * ,-----------------------------------------,                ,-----------------------------------------.
- * | Esc  |   1  |   2  |   3  |   4  |   5  |                |   6  |   7  |   8  |   9  |   0  | PgUp |
- * |------+------+------+------+------+-------                -------+------+------+------+------+------|
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |                |   Y  |   U  |   I  |   O  |   P  |   Ü  |
- * |------+------+------+------+------+-------                -------+------+------+------+------+------|
- * | F5   |   A  |   S  |   D  |   F  |   G  |                |   H  |   J  |   K  |   L  |   Ö  |  Ä   |
- * |------+------+------+------+------+------|------,  ,----  -------+------+------+------+------+------|
- * | <>|  |   Z  |   X  |   C  |   V  |   B  | Win |  | AltGr |   N  |   M  |   ,  |   .  |  -_  | PgDn |
- * |------+------+------+------+------+------|-------  --------------+------+------+------+------+------|
- * |  F1  | Ctrl | Alt  | Lower| Raise|      | Shift|  |  Spc |      | BckSp| Del  |  F10 |  F11 |  F12 |
- * `------------------------------------------------'  '------------------------------------------------'
+  /* Qwertz
+ * ,-----------------------------------------------------------------------------------------.
+ * | Esc |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  |  \  |  `  |
+ * |-----------------------------------------------------------------------------------------+
+ * | Tab    |  Q  |  W  |  E  |  R  |  T  |  Z  |  U  |  I  |  O  |  P  |  [  |  ]  |  |     |
+ * |-----------------------------------------------------------------------------------------+
+ * | Ctrl    |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  |  '  |     |  |    |
+ * |-----------------------------------------------------------------------------------------+
+ * | Shift |    |  Y  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  |    | Del |      |
+ * |-----------------------------------------------------------------------------------------+
+ * | Ctrl  | LGUI | LAlt  | Lower | Bcsp | Enter | Shift | Space | Raise |     |     |     |     |
+ * `-----------------------------------------------------------------------------------------'
  */
-[_QWERTY] = LAYOUT(
-   KC_ESC ,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,   KC_7,   KC_8,     KC_9,    KC_0,   KC_PGUP, \
-   KC_TAB ,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      DE_Z,   KC_U,   KC_I,     KC_O,    KC_P,    DE_UE, \
-   KC_F5  ,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,   KC_J,   KC_K,     KC_L,    DE_OE,   DE_AE, \
-   DE_LESS,  DE_Y,    KC_X,    KC_C,    KC_V,    KC_B,   KC_BSPC, KC_ENT,   KC_N,   KC_M,  KC_COMM,  KC_DOT, DE_MINS, KC_PGDN, \
-   KC_LCTL, KC_LALT, KC_LCMD, KC_LSFT, LOWER,           KC_LSFT, RSFT_T(KC_SPC),           RAISE, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
-
+  [_QWERTZ] = LAYOUT( /* Basic QWERTY */ 
+        KC_ESC,   DE_CIRC,    KC_1,    KC_2,    KC_3,  KC_4,   KC_5,              KC_6,    KC_7,    KC_8,    KC_9,    KC_0, DE_SS, DE_ACUT, 
+        KC_TAB,        DE_Q,    DE_W,    DE_E,    DE_R,    DE_T,           DE_Z,    DE_U,    DE_I,    DE_O,    DE_P,    DE_UE, DE_PLUS, 
+        KC_CAPS,        DE_A,    DE_S,    DE_D,    DE_F,    DE_G,           DE_H,    DE_J,    DE_K,    DE_L,    DE_OE,   DE_AE, DE_HASH, 
+        KC_LSFT, DE_LESS, DE_Y,    DE_X,    DE_C,    DE_V,           DE_B,    DE_N,    DE_M,    DE_COMM, DE_DOT,  DE_MINS,    KC_DEL, 
+        KC_LCTL, KC_LGUI, KC_LALT, LOWER,   KC_BSPC,  KC_ENT,         KC_RSFT, KC_SPC, RAISE, KC_DEL, KC_RALT, KC_RGUI, KC_RCTL 
 ),
-
-/* Lower */
-[_LOWER] = LAYOUT(
-   DE_CIRC, DE_EXLM ,DE_DQOT, DE_HASH, DE_DLR , DE_PERC,                   DE_AMPR, DE_SLSH, _______, _______, DE_EQL , _______, \
-   _______,  DE_AT , _______, DE_EURO, DE_LESS, DE_MORE,                   _______, DE_LBRC, DE_RBRC, DE_ASTR, _______, _______, \
-   _______, _______, _______, _______, DE_QST , DE_QUOT,                   DE_HASH, DE_LPRN, DE_RPRN, _______, _______, DE_QUOT, \
-   _______, _______, _______, _______, DE_EXLM, _______, KC_DEL , _______, _______, DE_LCBR, DE_RCBR, _______, _______, _______, \
-   KC_LCTL, KC_LALT, KC_LSFT, KC_LCMD,   LOWER,          _______, _______,           RAISE , _______, _______, _______, _______ \
+  [_LOWER] = LAYOUT( /* Basic QWERTY */
+        ______, ______, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
+        ______, ______,______, ______, ______, ______ ,         ______, KC_PSCR, KC_SLCK, KC_PAUS, KC_UP, ______, ______, \
+        ______, KC_VOLD,KC_VOLU,KC_MUTE,______, ______,         KC_PAST,KC_PSLS,KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT, KC_INS,  \
+        ______, ______, KC_MPRV,KC_MPLY,KC_MNXT,______,         ______, KC_PPLS,KC_PMNS,KC_END,  KC_PGDN, KC_DOWN, KC_DEL, \
+        ______, ______, ______, ______, ______, ______,         ______, ______, ______, ______, ______, ______, ______ \
 ),
-/*
-[_LOWER] = LAYOUT( \
-  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL, \
-  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,S(KC_NUHS),S(KC_NUBS),_______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
-),
-*/
-
-/* Raise */
-[_RAISE] = LAYOUT(
-   KC_F12 ,  KC_F1 ,  KC_F2 ,  KC_F3 ,  KC_F4 ,  KC_F5 ,                    KC_F6 ,  KC_F7 ,  KC_F8 ,  KC_F9 ,  KC_F10, KC_VOLU, \
-   _______, _______, _______,  KC_UP , _______, _______,                   KC_PGUP,  KC_7 ,  KC_8 ,  KC_9 , KC_PMNS,  KC_F11, \
-   _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______,                   KC_HOME,  KC_4 ,  KC_5 ,  KC_6 , KC_PPLS,  KC_F12, \
-   _______, _______, _______, _______, _______, _______, KC_DEL, _______, KC_PGDN,  KC_1 ,  KC_2 ,  KC_3 , KC_PENT, KC_VOLD, \
-   KC_LCTL, KC_LALT, KC_LCMD, KC_LSFT,   LOWER,         _______, _______,            RAISE,  KC_P0 , KC_MPRV, KC_MPLY, KC_MNXT \
+  [_RAISE] = LAYOUT( /* Basic QWERTY */
+        ______, ______, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
+        ______, ______,______, ______, ______, ______ ,         ______, KC_PSCR, KC_SLCK, KC_PAUS, KC_UP, ______, ______, \
+        ______, KC_VOLD,KC_VOLU,KC_MUTE,______, ______,         KC_PAST,KC_PSLS,KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT, KC_INS, \
+        ______, ______, KC_MPRV,KC_MPLY,KC_MNXT,______,         ______, KC_PPLS,KC_PMNS,KC_END,  KC_PGDN, KC_DOWN, KC_DEL, \
+        ______, ______, ______, ______, ______, ______,         ______, ______, ______, ______, ______, ______, ______ \
 )
-
-/*[_RAISE] = LAYOUT( \
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
-  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
-)*/
-
 };
 
+/*
 void persistent_default_layer_set(uint16_t default_layer) {
   eeconfig_update_default_layer(default_layer);
   default_layer_set(default_layer);
-}
+} */
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case QWERTY:
-      if (record->event.pressed) {
-        persistent_default_layer_set(1UL<<_QWERTY);
-      }
-      return false;
-      break;
     case LOWER:
       if (record->event.pressed) {
         layer_on(_LOWER);
